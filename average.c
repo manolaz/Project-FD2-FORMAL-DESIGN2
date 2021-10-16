a28d0828dabb68495aab378da732d1f49048ee6b
Tristan NGUYEN <trungupdate@gmail.com>
2021-10-16 21:32:23 +0700

    AVERAGE + SUM

---
+++ average.c
@@ -0,0 +1,17 @@
+/*@
+assigns sum;
+requires size > 0;
+requires \forall int i; 0 <= i < size ==> \valid (array + i);
+loop assigns i;
+loop invariant 0<=i<size;
+ensures \result == \sum(0,size−1,\lambda integer i; array[i])/size;
+*/
+float average(int ∗array , int size) 
+{
+	int sum=0;
+	for (int i = 0; i < size ; i++) 
+	{
+		sum = sum+array[i];
+	}
+	return sum/size;
+}
